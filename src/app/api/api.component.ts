b005f20119539a3e16c484ded88db2cb0765b123
malikromir <malikromir@gmail.com>
2021-07-24 12:04:50 +0530

    breadcrumb

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -14,36 +14,9 @@ export class ApiComponent implements AfterViewInit, OnInit {
   constructor(private apiService: ApiService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    console.log('hi');
     this.type = this.route.snapshot.data.breadcrumb;
-    console.log(this.type);
     this.spec = this.apiService.getApi(this.type);
   }
-
-  // this.route.data.subscribe(data => {
-  //   this.type = data.breadcrumb;
-  // switch (data.breadcrumb) {
-  //   case 'Pet': {
-  //     this.type = data.breadcrumb;
-  //     break;
-  //   }
-  //   case 'Store': {
-  //     this.type = data.breadcrumb;
-  //     break;
-  //   }
-  //   case 'User': {
-  //     this.type = data.breadcrumb;
-  //     break;
-  //   }
-  // }
-  // });
-  // console.log(this.type);
-  // this.route.queryParams.subscribe(params => {
-  //   this.type = params.type;
-  //   console.log(typeof this.type);
-  // });
-
-  // }
   ngAfterViewInit() {
     const ui = SwaggerUI({
       domNode: document.getElementById('swagger-ui'),
